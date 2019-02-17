f68c01f3bd03745acb64a6959b3ca9123fca6e3a
mustafcode10 <mustafdiiriye@hotmail.com>
2019-02-18 01:32:41 +0200

    login

--- forms.py
+++ forms.py
@@ -15,4 +15,4 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = StringField('Login')
+    submit = SubmitField('Login')
