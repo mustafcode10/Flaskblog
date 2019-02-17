5d72a0e12cf4f6c95bb980cca1490d2637fab367
mustafcode10 <mustafdiiriye@hotmail.com>
2019-02-18 00:54:53 +0200

    register and layout front end

--- flaskblog.py
+++ flaskblog.py
@@ -1,5 +1,8 @@
-from flask import Flask,render_template,url_for
+from flask import Flask,render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'a4ac4fd2fb80d138ab8a3a7412132d09'
 
 posts = [
     {
@@ -19,7 +22,7 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     # return "<h1>Home Page!<h1>"
     return render_template('home.html', posts=posts)
 
@@ -27,6 +30,19 @@ def hello():
 def about():
     # return "<h1>About Page!<h1>"   
     return render_template('about.html', title ='About')
+
+@app.route("/register", methods =['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for  {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title ='Register', form = form)  
+@app.route("/login")
+def login():
+    # return "<h1>About Page!<h1>" 
+    form = LoginForm()
+    return render_template('login.html', title ='Login', form = form)       
 
 
 if __name__=='__main__':
